f5e73e3135630c3202854c86d3a6f41d3b2724f3
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2022-02-11 21:34:17 +0300

    commit 11.02.22

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,11 +1,13 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
@@ -14,25 +16,30 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
 
-    SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-    Calendar calendar = new GregorianCalendar();
+
+    private String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
 
     @Test
     public void shouldRegByAccount() {
 
-        calendar.add(Calendar.DAY_OF_YEAR, 3);
-        String date = formatter.format(calendar.getTime());
+        Configuration.holdBrowserOpen = true;
+
+        String planningDate = generateDate(10);
 
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Волгоград");
-        $("[data-test-id=date] input").shouldHave(value(date));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Володин Евгений");
         $("[data-test-id=phone] input").setValue("+79218032784");
         $("[data-test-id=agreement] span").click();
         $(withText("Забронировать")).click();
         $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(11));
         $(".notification__title").shouldHave(text("Успешно!"));
-        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + planningDate));
     }
 
 }
